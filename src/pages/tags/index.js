5b4dd8c7ca1f7a39e5376282b1b31423d20ed283
YuryT <yury.tyn@hotmail.com>
2018-09-30 23:29:06 +0300

    customize tags

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,31 +1,15 @@
-/**
- * Created by vaibhav on 31/3/18
- */
 import React from 'react'
 import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
+import Header from '../../components/Header'
 
 const TagsPage = ({
   data: { allMarkdownRemark: { group }, site: { siteMetadata: { title } } },
 }) => (
   <div>
     <Helmet title={`Tags | ${title}`} />
-    <section className='hero is-primary is-bold'>
-      <div className='hero-body'>
-        <div className='container'>
-          <div className='columns'>
-            <div className='column is-10 is-offset-1'>
-              <div className='section'>
-                <h1 className='title'>
-                                    Tags
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Header title='Теги' />
     <section className='section'>
       <div className='container content'>
         <div className='columns'>

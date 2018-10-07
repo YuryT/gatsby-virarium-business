488a24289c4a7c9d188cd7ac906a239da3edade1
YuryT <yury.tyn@hotmail.com>
2018-10-07 21:20:01 +0300

    change manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,13 +78,13 @@ module.exports = {
         display: 'standalone',
         icons: [
           {
-            src: `/icons/icon-192x192.png`,
+            src: `/icons/android-chrome-192x192.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `/icons/icon-512x512.png`,
-            sizes: `512x512`,
+            src: `/icons/android-chrome-384x384.png`,
+            sizes: `384x384`,
             type: `image/png`,
           },
         ],

192f6751aee3f2cf094d54d48bd9b197f6a3b0d8
YuryT <yury.tyn@hotmail.com>
2018-10-13 01:38:09 +0300

    improve header menu

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import MapYandexStatic from './MapYandexStatic'
+import vk_logo from '../../img/vk-brands.svg'
+import instagram_logo from '../../img/instagram-brands.svg'
+import config from '../../../meta/config'
 
 const Contact = ({email, phone, description, title, schedule, map}) => {
   return (
@@ -20,6 +23,20 @@ const Contact = ({email, phone, description, title, schedule, map}) => {
         <p>
           <a href={`mailto:${email}`}>{email}</a>
         </p>
+        <div style={{margin: '10px 0'}}>
+          <p>
+            <a href={config.vk} target='_blank'>
+              <img src={vk_logo} alt='vk' style={{width: '15px'}}/>
+              <span> {config.vk} </span>
+            </a>
+          </p>
+          <p>
+            <a href={config.instagram} target='_blank'>
+              <img src={instagram_logo} alt='vk' style={{width: '15px'}}/>
+              <span> {config.instagram}</span>
+            </a>
+          </p>
+        </div>
       </div>
       <div className='column is-6-desktop'>
         {map ? (<MapYandexStatic href={map.href} src={map.src} />) : null}
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,68 +1,83 @@
-import React from 'react'
+import React,  { Component } from 'react'
+
 import Link from 'gatsby-link'
 import logo from '../../img/logo.svg'
 import vk_logo from '../../img/vk-brands.svg'
 import instagram_logo from '../../img/instagram-brands.svg'
+import config from '../../../meta/config'
 
-const NavBar = () => {
-  return (
-    <nav className='navbar is-fixed-top' aria-label='main navigation'>
-      <div className='navbar-brand'>
-        <Link to='/' className='navbar-item'>
-          <figure className='mage'>
-            <img src={logo} alt='ViRarium' style={{ width: '125px' }} />
-          </figure>
-        </Link>
-        <button className='button navbar-burger' data-target='navMenu'>
-          <span />
-          <span />
-          <span />
-        </button>
-      </div>
-      <div className='navbar-menu' id='navMenu'>
-        <div className='navbar-start'>
-          <Link className='navbar-item' to='/pricing' >
-                        Цены
+class NavBar extends Component {
+  constructor(props) {
+    super(props)
+    const simpleLinks = [
+      { to: '/pricing', text: 'Цены' },
+      { to: '/quests', text: 'Квесты' },
+      { to: '/birthday', text: 'Дни рождения' },
+      { to: '/blog', text: 'Новости' },
+      { to: '/faq', text: 'FAQ' },
+    ]
+    this.state = {
+      menuIsActive: false,
+      simpleLinks,
+    }
+    this.closeMenu = this.closeMenu.bind(this)
+  }
+
+  closeMenu () {
+    const $navMenu = document.getElementById('navMenu')
+    // check in ie
+    if ($navMenu.classList.remove) {
+      $navMenu.classList.remove('is-active')
+    }
+  }
+
+  render () {
+    return (
+      <nav className='navbar is-fixed-top' aria-label='main navigation'>
+        <div className='navbar-brand'>
+          <Link to='/' className='navbar-item'>
+            <figure className='mage'>
+              <img src={logo} alt='ViRarium' style={{width: '125px'}} />
+            </figure>
           </Link>
-          <Link className='navbar-item' to='/quests' >
-                        Квесты
-          </Link>
-          <Link className='navbar-item' to='/birthday'>
-                        Дни рождения
-          </Link>
-          <Link className='navbar-item' to='/other-services'>
-                        Другие услуги
-          </Link>
-          <Link className='navbar-item' to='/blog'>
-                        Новости
-          </Link>
-          <Link className='navbar-item' to='/faq'>
-                        FAQ
-          </Link>
+          <button className='button navbar-burger' data-target='navMenu'>
+            <span />
+            <span />
+            <span />
+          </button>
         </div>
-        <div className='navbar-end'>
-          <div className='navbar-item'>
-            <div className='field is-grouped'>
-              <p className='control'>
-                <a href='https://vk.com/virarium' target='_blank'>
-                  <img src={vk_logo} alt='vk' style={{ width: '50px' }} />
-                </a>
-                <a href='https://www.instagram.com/virarium_vr_club/' target='_blank'>
-                  <img src={instagram_logo} alt='vk' style={{ width: '50px' }} />
-                </a>
-                <Link
-                  className='button is-primary is-outlined'
-                  activeClassName="active-menu"
-                  to='/contact'>
-                                    Контакты
-                </Link>
-              </p>
+        <div className='navbar-menu' id='navMenu'>
+          <div className='navbar-start'>
+            {this.state.simpleLinks.map((l, index) => (
+              <Link key={index} className='navbar-item' to={l.to} onClick={this.closeMenu} >
+                {l.text}
+              </Link>
+            ))}
+          </div>
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              <div className='field is-grouped'>
+                <p className='control'>
+                  <a href={config.vk} target='_blank'>
+                    <img src={vk_logo} alt='vk' style={{width: '50px'}}/>
+                  </a>
+                  <a href={config.instagram} target='_blank'>
+                    <img src={instagram_logo} alt='vk' style={{width: '50px'}}/>
+                  </a>
+                  <Link
+                    className='button is-primary is-outlined'
+                    activeClassName="active-menu"
+                    to='/contact'>
+                    Контакты
+                  </Link>
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
 
 export default NavBar

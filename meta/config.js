9918b8cff23814a733ac49903fc19df7363cbdc3
YuryT <yury.tyn@hotmail.com>
2018-10-12 16:18:46 +0300

    config site fix

--- meta/config.js
+++ meta/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteTitle: 'Клуб виртуальной реальности ViRarium', // Site title.
   siteTitleAlt: 'ViRarium', // Alternative site title for SEO.
-  siteLogo: '/logos/logo-512x512.png', // Logo used for SEO and manifest.
+  siteLogo: '/logos/logo-512x512.png', // todo Logo used for SEO and manifest.
   siteUrl: 'https://virarium.ru', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Клуб виртуальной реальности ViRarium', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: 'GTM-544SPTQ', // GTM tracking ID.
@@ -13,7 +13,7 @@ module.exports = {
   userTwitter: 'virarium',
   userLocation: 'TODO',
   userDescription: '',
-  copyright: 'Copyright © 2018 ViRarium club.', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © 2018 ViRarium VR club.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#003a83', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
   email: 'virarium-vrn@yandex.ru',
